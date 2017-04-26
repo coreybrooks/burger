860d39cecc1e26a7aaf0dcae8b5b91c65366d188
Corey <coreybrooks2@gmail.com>
2017-04-25 19:55:08 -0500

    file setup and first draft

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ("mushroom-swiss");
+INSERT INTO burgers (burger_name, devoured) VALUES ("bacon", true);
+INSERT INTO burgers (burger_name) VALUES ("veggie");
+INSERT INTO burgers (burger_name) VALUES ("stuffed");
